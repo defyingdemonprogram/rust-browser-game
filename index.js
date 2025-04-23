1aded8b04b0b07d77bed2902a2c791ab66c451f6
realsanjeev2 <realsanjeev2@gmail.com>
2025-04-24 01:30:58 +0545

    Implement entitties for player and enimies

--- index.js
+++ index.js
@@ -10,12 +10,19 @@ async function start() {
     );
     const memoryView = new Uint8Array(game.instance.exports.memory.buffer);
 
+    game.instance.exports.init();
     const displayAddr = game.instance.exports.get_display();
     const displayWidth = game.instance.exports.get_display_width();
     const displayHeight = game.instance.exports.get_display_height();
     const displaySize = displayWidth * displayHeight;
 
     const gameCanvas = document.getElementById("game-canvas");
+    document.addEventListener('keydown', e => {
+        console.log(e);
+        if (e.code === 'Space') {
+            game.instance.exports.toggle_pause();
+        }
+    });
     gameCanvas.addEventListener("mousemove", e => {
         game.instance.exports.mouse_move(e.offsetX, e.offsetY);
     });
